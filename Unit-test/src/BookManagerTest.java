e7a0f842afdf0ab5edd08dca5467de76109fd163
10000001a <pgdn06288888@gmail.com>
2024-06-01 17:25:43 +0900

    commit mission 1 which was done in class

---
+++ Unit-test/src/BookManagerTest.java
@@ -0,0 +1,67 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BookManagerTest {
+	BookManager bookManager;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		bookManager = new BookManager();
+	}
+
+	@Test
+	void addBook() {
+		Book book = new Book("1", "자바 기초", "Jane", 2021);
+
+		bookManager.addBook(book);
+
+		assertEquals(bookManager.books.size(), 1);
+		assertEquals(book.id, bookManager.books.get(0).id);
+	}
+
+	@Test
+	void addExistBook() {
+		Book book1 = new Book("1", "자바 기초", "Jane", 2021);
+
+		bookManager.addBook(book1);
+
+		assertThrows(IllegalArgumentException.class, () -> bookManager.addBook(book1));
+
+	}
+
+	@Test
+	void searchBook() {
+		Book book1 = new Book("1", "자바 기초", "Jane", 2021);
+
+		bookManager.addBook(book1);
+
+		String targetId = "1";
+		Book targetBook = bookManager.searchBook(targetId);
+
+		assertEquals(book1.id, targetBook.id);
+	}
+
+	@Test
+	void searchUnexistBook() {
+		String targetId = "1";
+
+		assertThrows(IllegalArgumentException.class, () -> bookManager.searchBook(targetId));
+	}
+
+	@Test
+	void removeBook() {
+		Book book1 = new Book("1", "자바 기초", "Jane", 2021);
+		String targetId = "1";
+
+		bookManager.addBook(book1);
+
+		bookManager.removeBook(targetId);
+
+		assertEquals(bookManager.books.size(), 0);
+		assertThrows(IllegalArgumentException.class, () -> bookManager.searchBook(targetId));
+	}
+
+}
